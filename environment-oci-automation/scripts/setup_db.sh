d8d032b298dc6a70b82403d78f12435095197b5f
Cesar Rios <rioscesar7@gmail.com>
2018-04-05 12:44:56 -0500

    Update setup_db.sh

--- environment-oci-automation/scripts/setup_db.sh
+++ environment-oci-automation/scripts/setup_db.sh
@@ -1,2 +1,2 @@
-sudo bash -c "chmod 777 /tmp/StateInsurance.sql && mv /tmp/StateInsurance.sql /home/oracle/home"
-sudo su - oracle -c "export ORACLE_SID=aTFdb && export ORAENV_ASK=NO && . /usr/local/bin/oraenv && exit | sqlplus / as sysdba @StateInsurance.sql"
+sudo bash -c "mv /tmp/StateInsurance.sql /home/oracle/ && chmod 777 /home/oracle/StateInsurance.sql"
+sudo su - oracle -c "export ORACLE_SID=aTFdb && export ORAENV_ASK=NO && . /usr/local/bin/oraenv && exit | sqlplus / as sysdba @/home/oracle/StateInsurance.sql"
